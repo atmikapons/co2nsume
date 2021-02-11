eee858b9e7f235c737c3d33063b12271f58c47e2
smoy99 <moy.samantha@outlook.com>
2021-02-10 22:50:08 -0500

    All placeholder pages connected to home & added back buttons to home.

--- routes/home.js
+++ routes/home.js
@@ -16,5 +16,66 @@ var router = function (app, db) {
 		}
 		response.end();
 	});
+
+    //Click menu button, go to dining hall menu suggestion page (menu.ejs)
+    app.get('/menu', function(request, response) {
+		if (request.session.loggedin) {
+			response.render('pages/menu');
+		} else {
+			response.send('Please login to view this page!');
+		}
+		response.end();
+	});
+
+    //Click store button, go to store page (store.ejs)
+    app.get('/store', function(request, response) {
+		if (request.session.loggedin) {
+			response.render('pages/store');
+		} else {
+			response.send('Please login to view this page!');
+		}
+		response.end();
+	});
+
+    //Click stats button, go to stats page (stats.ejs)
+    app.get('/stats', function(request, response) {
+		if (request.session.loggedin) {
+			response.render('pages/stats');
+		} else {
+			response.send('Please login to view this page!');
+		}
+		response.end();
+	});
+
+    //Click info button, go to info page (info.ejs)
+    app.get('/info', function(request, response) {
+		if (request.session.loggedin) {
+			response.render('pages/info');
+		} else {
+			response.send('Please login to view this page!');
+		}
+		response.end();
+	});
+
+    //Click points button, go to points page (points.ejs)
+    app.get('/points', function(request, response) {
+		if (request.session.loggedin) {
+			response.render('pages/points');
+		} else {
+			response.send('Please login to view this page!');
+		}
+		response.end();
+	});
+
+    //Click back button, go to home page (home.ejs)
+    app.get('/home', function(request, response) {
+		if (request.session.loggedin) {
+			response.render('pages/home');
+		} else {
+			response.send('Please login to view this page!');
+		}
+		response.end();
+	});
+
 }
 module.exports=router;
