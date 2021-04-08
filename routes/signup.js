bac14ff7def8cf862d7ac29a7fd7bd4e894b309a
NSSMCS214 <nbs66@scarletmail.rutgers.edu>
2021-04-08 11:42:01 -0400

    fixed sign in page and added demo account

--- routes/signup.js
+++ routes/signup.js
@@ -8,22 +8,28 @@ var app = express();
 var router = function (app, db) {
 
     //post request for signup page
-    app.post('/signup', function (req, res) {
+    app.post('/sign', function (req, res) {
         let username = req.body.username;
+        db.query('SELECT * FROM `User Info` WHERE `Username` = ? ', [username], function(error, results, fields) {
+            if (results.length > 0) {
+                res.send("Username already in use");
+                res.end();
+            }
+        });
         let first = req.body.first;
         let last = req.body.last;
         let password = req.body.password;
-        let values = [[username, first, last, password]];
-        if (username != '' || first != '' || last != '' || password != '') {
-            let addQuery = "INSERT INTO `User Info` (Username, First, Last, Password) VALUES ?";
+        let values = [[username, first, last, password, 0]];
+        if (username && first && last && password) {
+            let addQuery = "INSERT INTO `User Info` (Username, First, Last, Password, Carbon) VALUES ?";
             db.query(addQuery, [values], function (err, result) {
                 if (err) {
                     return res.status(500).send(err);
                 }
                 else {
-                    return res.status(200).send({
-                        'status': "Success",
-                    });
+                    req.session.loggedin = true;
+					req.session.username = username;
+					res.redirect('/login');
                 }
             });
         }
